36d6dc037fe490c755287fdf1a41362b4253c27e
Vikram G S <Sujay.VikramGS@team.telstra.com>
2025-06-17 18:58:58 +0530

    initial commit

---
+++ SimPorter/src/main/java/com/example/SimPorter/SimPorterApplication.java
@@ -0,0 +1,13 @@
+package com.example.SimPorter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimPorterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimPorterApplication.class, args);
+	}
+
+}
---
+++ SimPorter/src/test/java/com/example/SimPorter/SimPorterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SimPorter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimPorterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
